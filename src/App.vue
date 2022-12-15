1588406a0cbde5350f492901d372d91f053a53c2
Luis Guerra <guerra.lfd@gmail.com>
2022-12-14 21:03:25 -0500

    arreglados los problemas de tailwind y swiper

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,40 @@
-
 <template>
-  <h1 className="text-3xl font-bold underline">
-      Manito, listo para comenzar con el proyeSEXO
-  </h1>
+  <!-- <section v-for="(item, index) in items" :key="index" class=" bg-slate-600 text-lg text-red-700 h-screen w-full font-extrabold text-center border-y-4">repeticion {{index}}</section> -->
+
+  <swiper 
+    class="h-screen"
+    :direction="'vertical'"
+    :modules="modules"
+    :mousewheel="true"
+    :slides-per-view="1"
+    :space-between="0"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide v-for="(item, index) in items" :key="index" class=" bg-slate-600 text-lg text-red-700 h-screen w-full font-extrabold text-center border-y-4">
+      repeticion {{index}}
+    </swiper-slide>
+
+  </swiper>
 </template>
 
+<script setup>
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+  import { Mousewheel } from 'swiper';
+   // Import Swiper styles
+  import 'swiper/css/bundle';
+
+    const modules = [Mousewheel]
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log('slide change');
+    };
+
+
+  const items = [
+    1,2,3,4
+  ]
+
+</script>
