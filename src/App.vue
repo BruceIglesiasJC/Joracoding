44520af5d956b885859f8b3a758ef7ce8be8755d
Luis Guerra <guerra.lfd@gmail.com>
2022-12-15 22:23:41 -0500

    pipipi

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,43 @@
 <template>
-  <!-- <section v-for="(item, index) in items" :key="index" class=" bg-slate-600 text-lg text-red-700 h-screen w-full font-extrabold text-center border-y-4">repeticion {{index}}</section> -->
 
-  <swiper 
-    class="h-screen"
-    :direction="'vertical'"
-    :modules="modules"
-    :mousewheel="true"
-    :slides-per-view="1"
-    :space-between="0"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
-  >
-    <swiper-slide v-for="(item, index) in items" :key="index" class=" bg-slate-600 text-lg text-red-700 h-screen w-full font-extrabold text-center border-y-4">
-      repeticion {{index}}
-    </swiper-slide>
+    <NavbarVue class="" ></NavbarVue>
+    <div class=" h-screen">
+      <swiper 
+      class=" h-full"
+      :direction="'vertical'"
+      :modules="modules"
+      :mousewheel="true"
+      :slides-per-view="1"
+      :space-between="0"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+      >
+        <swiper-slide v-for="(item, index) in items" :key="index" class=" bg-slate-600 text-lg text-red-700 w-full    font-extrabold text-center">
+          <h2 class=" mt-5">
+            repeticion {{item}}
+          </h2>
+        </swiper-slide>
+      </swiper>
+    </div>
 
-  </swiper>
+
 </template>
 
 <script setup>
   import { Swiper, SwiperSlide } from 'swiper/vue';
   import { Mousewheel } from 'swiper';
-   // Import Swiper styles
   import 'swiper/css/bundle';
+  import NavbarVue from './components/Navbar.vue';
+  
+  const modules = [Mousewheel]
+  const onSwiper = (swiper) => {
+    console.log(swiper);
+  };
+  const onSlideChange = () => {
+    console.log('slide change');
+  };
 
-    const modules = [Mousewheel]
-    const onSwiper = (swiper) => {
-      console.log(swiper);
-    };
-    const onSlideChange = () => {
-      console.log('slide change');
-    };
-
-
+  
   const items = [
     1,2,3,4
   ]
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class=" bg-slate-900 w-full fixed top-0 left-0 z-10 flex justify-between text-white">
+        <div>
+            <img src="" alt="">
+        </div>
+
+        <div>
+            <ul>
+                <li>
+                    <a  href="">Prueba</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+    
+</script>
